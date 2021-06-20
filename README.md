7c4b8c6007e25c3806895800a97ccdc91e21949b
Chitsanupong <first30952@gmail.com>
2021-06-20 11:11:16 +0700

    add geocoding

--- README.md
+++ README.md
@@ -8,7 +8,16 @@
 **Tips:** setTimeout() is not default provided in JavaScript or V8 but Node.js use C++ to create this function.
 
 ## Make HTTP request
-1. use [weatherstack API](https://weatherstack.com/) to send request for weather information. These following are steps to use weatherstack API
-2. use *request* npm package to make HTTP request but now it's deprecated, you can use the *postman-request* instead.
-3. 
-   
+1. Use [weatherstack API](https://weatherstack.com/) to send request for weather information. These following are steps to use weatherstack API
+2. Use *request* npm package to make HTTP request but now it's deprecated, you can use the *postman-request* instead.
+3. Make get request to weatherstack API by send latitude and longitude.
+
+## Geocoding
+We can receive address from the user and transfer to weather data of that address. We can use [mapbox](https://www.mapbox.com/) to use their API. 
+1. Sign up mapbox account and go to tokens section
+2. You can use access tokens to send API
+3. In API documentation, go to [Geocoding](https://docs.mapbox.com/api/search/geocoding/) section and forward geocoding is what we will use.
+   1. forward geocoding - convert from address to lat/long
+   2. reverse geocoding - convert from lat/long to address
+
+## 
--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 const request = require("postman-request");
 
-const url =
+const weatherUrl =
   "http://api.weatherstack.com/current?access_key=598ef13bdf9cb2d66807269ddcab0a4b&query=37.8267,-122.4233";
 
-request({ url: url, json: true }, (error, response) => {
-  console.log("error:", error);
-  console.log("statusCode:", response && response.statusCode);
-  console.log("data:", response.body.current);
+request({ url: weatherUrl, json: true }, (error, response) => {
+  if (error) {
+    console.log("weatherstack: Unable to connect to weather service!");
+  } else if (error.body.error) {
+    console.log("weatherstack: Unable to find location. Try another location.");
+  } else {
+    console.log(
+      response.body.current.weather_descriptions[0] +
+        ". It is currently " +
+        response.body.current.temperature +
+        " degrees out." +
+        " It feels like " +
+        response.body.current.feelslike +
+        " degrees out."
+    );
+  }
+});
+
+// Geocoding
+// input: address -> lat/long -> output: weather
+const geocodeUrl =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibmV2ZXJyZXN0IiwiYSI6ImNrcTRtYjUydjB1eDUyd212azA4Z2dmM2IifQ.khy_M242W6MKufitNCdKxw";
+
+request({ url: geocodeUrl, json: true }, (error, response) => {
+  if (error) {
+    console.log("geocode: Unable to connect to location services");
+  } else if (response.body.features.length === 0) {
+    console.log("geocode: Unable to find location. Try another location.");
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log("latitude: " + latitude + ", longitude: " + longitude);
+  }
 });
