60e744a0ac16c23ca879cb1a79f9c78e10a2eaac
firstneverrest <c.tangvasinkul.com>
2021-06-21 20:13:57 +0700

    code splitting

--- app.js
+++ app.js
@@ -1,39 +1,33 @@
 const request = require("postman-request");
+const geocode = require("./utils/geocode");
 
-const weatherUrl =
-  "http://api.weatherstack.com/current?access_key=598ef13bdf9cb2d66807269ddcab0a4b&query=37.8267,-122.4233";
+// const weatherUrl =
+//   "http://api.weatherstack.com/current?access_key=598ef13bdf9cb2d66807269ddcab0a4b&query=37.8267,-122.4233";
 
-request({ url: weatherUrl, json: true }, (error, response) => {
-  if (error) {
-    console.log("weatherstack: Unable to connect to weather service!");
-  } else if (error.body.error) {
-    console.log("weatherstack: Unable to find location. Try another location.");
-  } else {
-    console.log(
-      response.body.current.weather_descriptions[0] +
-        ". It is currently " +
-        response.body.current.temperature +
-        " degrees out." +
-        " It feels like " +
-        response.body.current.feelslike +
-        " degrees out."
-    );
-  }
+// request({ url: weatherUrl, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("weatherstack: Unable to connect to weather service!");
+//   } else if (error.body.error) {
+//     console.log("weatherstack: Unable to find location. Try another location.");
+//   } else {
+//     console.log(
+//       response.body.current.weather_descriptions[0] +
+//         ". It is currently " +
+//         response.body.current.temperature +
+//         " degrees out." +
+//         " It feels like " +
+//         response.body.current.feelslike +
+//         " degrees out."
+//     );
+//   }
+// });
+
+weather(37.8267, -122.4233, (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
 });
 
-// Geocoding
-// input: address -> lat/long -> output: weather
-const geocodeUrl =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibmV2ZXJyZXN0IiwiYSI6ImNrcTRtYjUydjB1eDUyd212azA4Z2dmM2IifQ.khy_M242W6MKufitNCdKxw";
-
-request({ url: geocodeUrl, json: true }, (error, response) => {
-  if (error) {
-    console.log("geocode: Unable to connect to location services");
-  } else if (response.body.features.length === 0) {
-    console.log("geocode: Unable to find location. Try another location.");
-  } else {
-    const latitude = response.body.features[0].center[1];
-    const longitude = response.body.features[0].center[0];
-    console.log("latitude: " + latitude + ", longitude: " + longitude);
-  }
-});
+// geocode("Philadelphia", (error, data) => {
+//   console.log("Error", error);
+//   console.log("Data", data);
+// });
